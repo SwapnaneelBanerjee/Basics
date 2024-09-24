820fd8d7d3e18a26bbc7168fe4877fb81ba5852c
Swapnaneel Banerjee <144154323+SwapnaneelBanerjee@users.noreply.github.com>
2024-09-24 18:55:20 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,1 +1,37 @@
+//Binary Search
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {2, 3, 4, 10, 40}; 
+        int target = 10; 
+
+        int result = binarySearch(array, target); 
+        if (result == -1) {
+            System.out.println("Element not found in the array");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0; 
+        int right = arr.length - 1; 
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            if (arr[mid] == target) {
+                return mid; 
+            }
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; 
+    }
+}
 
