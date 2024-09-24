b163310a525c7c611b814e795040aadcdd83966b
Swapnaneel Banerjee <144154323+SwapnaneelBanerjee@users.noreply.github.com>
2024-09-24 18:46:23 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,1 +1,32 @@
 //Bubblesort 
+import java.util.*;
+class ABC{
+public static void main(String[] args) {
+  int[] a = {1,3,4,2,8,5,7,9,12,10};
+  System.out.println("Original Array: " + Arrays.toString(array));
+        
+        bubbleSort(array);
+
+        System.out.println("Sorted Array: " + Arrays.toString(array));
+    }
+
+  public static void bubbleSort(int[] a){
+     int n = arr.length;
+     boolean swapped;
+    for (int i = 0; i < n - 1; i++) {
+            swapped = false; 
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+}
+     if (!swapped) {
+                break;
+            }
+    }
+  }
+}
